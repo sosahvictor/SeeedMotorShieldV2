fd9c3812ae0581279084bd3900aac8a9e043fb12
loovee <luweicong@seeed.cc>
2016-06-01 16:37:20 +0800

    fix a bug that ch1 don't work

--- MotorDriver.cpp
+++ MotorDriver.cpp
@@ -21,14 +21,16 @@
 #include "MotorDriver.h"
 #include "seeed_pwm.h"
 
+#define PWM_FREQ    10000
+
 void MotorDriver::begin()
 {
     speed0 = 0;
     speed1 = 0;
-	pinMode(MOTORSHIELD_IN1,OUTPUT);
-	pinMode(MOTORSHIELD_IN2,OUTPUT);
-	pinMode(MOTORSHIELD_IN3,OUTPUT);
-	pinMode(MOTORSHIELD_IN4,OUTPUT);
+	pinMode(MOTORSHIELD_IN1, OUTPUT);
+	pinMode(MOTORSHIELD_IN2, OUTPUT);
+	pinMode(MOTORSHIELD_IN3, OUTPUT);
+	pinMode(MOTORSHIELD_IN4, OUTPUT);
     digitalWrite(MOTORSHIELD_IN1, LOW);
     digitalWrite(MOTORSHIELD_IN2, LOW);
     digitalWrite(MOTORSHIELD_IN3, LOW);
@@ -68,16 +70,32 @@ void MotorDriver::brake(unsigned char motor_id)
 void MotorDriver::speed(int motor_id, int _speed)
 {
     if(motor_id<0 || motor_id>1)return;
-    
+
     _speed = _speed<-100 ? -100 : _speed;
     _speed = _speed>100 ? 100 : _speed;
     
-    if(motor_id == 0 && speed0 == _speed)return;
-    else speed0 = _speed;
-    
-    if(motor_id == 1 && speed1 == _speed)return;
-    else speed1 = _speed;
-    
+    if(motor_id == 0 && speed0 == _speed)
+    {
+        if(speed0 == _speed)
+        {
+            return ;
+        }
+        else
+        {
+            speed0 = _speed;
+        }
+    }
+    else if(motor_id == 1 && speed1 == _speed)
+    {
+        if(speed1 == _speed)
+        {
+            return ;
+        }
+        else 
+        {
+            speed1 = _speed;
+        }
+    }
     switch(motor_id)
     {
         case 0:
@@ -86,19 +104,19 @@ void MotorDriver::speed(int motor_id, int _speed)
         {
             digitalWrite(MOTORSHIELD_IN1, HIGH);
             digitalWrite(MOTORSHIELD_IN2, LOW);
-            PWM.setPwm(9, _speed, 10000);
+            PWM.setPwm(9, _speed, PWM_FREQ);
         }
         else if(_speed<0)
         {
             digitalWrite(MOTORSHIELD_IN1, LOW);
             digitalWrite(MOTORSHIELD_IN2, HIGH);
-            PWM.setPwm(9, 0-_speed, 10000);
+            PWM.setPwm(9, 0-_speed, PWM_FREQ);
         }
         else
         {
             digitalWrite(MOTORSHIELD_IN1, LOW);
             digitalWrite(MOTORSHIELD_IN2, LOW);
-            PWM.setPwm(9, _speed, 10000);
+            PWM.setPwm(9, _speed, PWM_FREQ);
         }
         
         break;
@@ -109,19 +127,19 @@ void MotorDriver::speed(int motor_id, int _speed)
         {
             digitalWrite(MOTORSHIELD_IN3, HIGH);
             digitalWrite(MOTORSHIELD_IN4, LOW);
-            PWM.setPwm(9, _speed, 10000);
+            PWM.setPwm(10, _speed, PWM_FREQ);
         }
         else if(_speed<0)
         {
             digitalWrite(MOTORSHIELD_IN3, LOW);
             digitalWrite(MOTORSHIELD_IN4, HIGH);
-            PWM.setPwm(9, 0-_speed, 10000);
+            PWM.setPwm(10, 0-_speed, PWM_FREQ);
         }
         else
         {
             digitalWrite(MOTORSHIELD_IN3, LOW);
             digitalWrite(MOTORSHIELD_IN4, LOW);
-            PWM.setPwm(9, _speed, 10000);
+            PWM.setPwm(10, _speed, PWM_FREQ);
         }
         break;
         
